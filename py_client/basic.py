5d0fa201feb04d73eec164e6d9429e5669a2f889
Ivan <ivan.osipchyk@hva.nl>
2022-12-06 12:43:10 +0100

    created first api request

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,6 +1,8 @@
 import requests
 
-endpoint = "http://localhost:8000/" #"http://127.0.0.1:8000/"
+endpoint = "http://localhost:8000/api/" #"http://127.0.0.1:8000/"
 
-print(requests.get(endpoint).text)
-print(requests.get(endpoint).status_code)
+get_response = requests.get(endpoint, json={"query": "Hello world"})
+print(get_response.text)
+print(get_response.status_code)
+print(get_response.json())
